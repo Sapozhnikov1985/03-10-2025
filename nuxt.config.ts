2bc157082a9e5cc0425d323cdc1013b90b2fd15f
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-03-28 15:30:46 +0300

    chore: dependency maintenance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,12 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
     modules: [
-      "@nuxtjs/tailwindcss",
-      "shadcn-nuxt",
-      "@nuxtjs/color-mode",
-      "@nuxt/icon",
-      "@nuxt/content",
-      "nuxt-aos",
+        "@nuxtjs/tailwindcss",
+        "shadcn-nuxt",
+        "@nuxtjs/color-mode",
+        "@nuxt/icon",
+        "@nuxt/content",
+        "nuxt-aos",
     ],
     css: ["@/assets/css/tailwind.css", "@/assets/css/global.scss"],
     shadcn: {
