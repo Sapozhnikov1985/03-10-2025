c4a39e4b0072bee67a388efdea32f0a36423d93f
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-04-20 14:44:56 +0300

    feat: implement video embed dialog

---
+++ components/common/VideoDialog.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+defineProps<{
+    name: string;
+    id: number;
+}>();
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger>
+            <Card>
+                <CardHeader class="space-y-2">
+                    <img />
+                    <CardTitle>{{ name }}</CardTitle>
+                </CardHeader>
+            </Card>
+        </DialogTrigger>
+        <DialogContent class="sm:max-w-[1000px]">
+            <DialogHeader>
+                <DialogTitle>{{ name }}</DialogTitle>
+            </DialogHeader>
+
+            <iframe
+                class="w-full aspect-video rounded-sm"
+                :src="`https://vkvideo.ru/video_ext.php?oid=-92204627&id=${id}&hd=2&autoplay=1`"
+                allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+                frameborder="0"
+                allowfullscreen
+            />
+        </DialogContent>
+    </Dialog>
+</template>
+
+<style lang="scss" scoped></style>
